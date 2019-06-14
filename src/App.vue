7957d43d55ab9f30e5fe6db8cd27a81c802da2bd
Peter <dante383@protonmail.com>
2019-06-14 02:14:37 +0200

    feed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-toolbar class="md-primary">
+        <h1 class="md-title">SnippetShare</h1>
+    </md-toolbar>
+ 
+    <div v-for="(snippet, $index) in snippets" :key="$index">
+    	<md-content>
+    		<snippet></snippet>
+    	</md-content>
+    </div>
+    <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+
+    <!--</md-content>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Snippet from './components/Snippet.vue'
+import InfiniteLoading from 'vue-infinite-loading';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Snippet, InfiniteLoading
+  },
+  data() {
+  	return {
+  		page: 1,
+  		snippets: [1,2,3]
+  	}
+  },
+  methods: {
+  	infiniteHandler ($state) {
+  		this.page += 1;
+  		this.snippets.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+  		$state.loaded();
+  		$state.complete();
+  	}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.md-content {
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Snippet.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="snippet">
+    <md-card>
+      <md-card-header>
+        <md-card-header-text>
+          <md-avatar class="md-avatar-icon">A</md-avatar>
+
+          <div class="md-title">Hello World in VueJs</div>
+          <div class="md-subhead"><md-chip>vuejs</md-chip><md-chip>javascript</md-chip><md-chip>nodejs</md-chip></div>
+        </md-card-header-text>
+
+        <md-menu md-size="big" md-direction="bottom-end">
+          <md-button class="md-icon-button" md-menu-trigger>
+            <md-icon>more_vert</md-icon>
+          </md-button>
+
+          <md-menu-content>
+            <md-menu-item @click="doACall">
+              <span>Call</span>
+              <md-icon>phone</md-icon>
+            </md-menu-item>
+
+            <md-menu-item @click="sendMessage">
+              <span>Send a message</span>
+              <md-icon>message</md-icon>
+            </md-menu-item>
+          </md-menu-content>
+        </md-menu>
+      </md-card-header>
+
+      <md-card-media>
+        <img src="https://cdn-images-1.medium.com/max/1200/1*HyGwWABsk9likhgSGfn1Sg.jpeg" alt="COBOL">
+      </md-card-media>
+
+      <md-card-content>
+        Simple Hello World component.
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button>View</md-button>
+        <md-button>Copy</md-button>
+      </md-card-actions>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'snippet',
+  props: {
+    title: String
+  },
+  components: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.md-card {
+    margin: 4px;
+    display: block;
+    vertical-align: top;
+  }
+
+  .md-card-example {
+    .md-subhead {
+      .md-icon {
+        $size: 16px;
+
+        width: $size;
+        min-width: $size;
+        height: $size;
+        font-size: $size !important;
+      }
+
+      span {
+        vertical-align: middle;
+      }
+    }
+
+    .card-reservation {
+      margin-top: 8px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .md-icon {
+        margin: 8px;
+      }
+    }
+
+    .md-button-group {
+      display: flex;
+
+      .md-button {
+        min-width: 60px;
+        border-radius: 2px;
+      }
+    }
+  }
+</style>
